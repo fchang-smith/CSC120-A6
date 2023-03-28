3a0e900070de3391fa8278028d113f9a1fcdc646
Feiran Chang <fchang@smith.edu>
2023-03-27 20:12:19 -0400

    Scaramouche my wife XD

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,41 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building{
 
-    public Cafe() {
+    private int nCoffeeOunces = 10; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets = 10; // The number of sugar packets remaining in inventory
+    private int nCreams = 10; // The number of "splashes" of cream remaining in inventory
+    private int nCups = 10; // The number of cups remaining in inventory
+
+    public Cafe(String name, String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nFloors);
+        if (nCoffeeOunces != -1) { this.nCoffeeOunces = nCoffeeOunces; }
+        if (nSugarPackets != -1) { this.nSugarPackets = nSugarPackets; }
+        if (nCreams != -1) { this.nCreams = nCreams; }
+        if (nCups != -1) { this.nCups = nCups; }
         System.out.println("You have built a cafe: ☕");
     }
-    
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (this.nCoffeeOunces < nCoffeeOunces) {throw new RuntimeException("Need " + nCoffeeOunces + "coffee ounces, but only have " + this.nCoffeeOunces); }
+        if (this.nSugarPackets < nSugarPackets) {throw new RuntimeException("Need " + nSugarPackets + "sugar packets, but only have " + this.nSugarPackets); }
+        if (this.nCreams < nCreams) {throw new RuntimeException("Need " + nCreams + "Creams, but only have " + this.nCreams); }
+        if (this.nCups < 1) {throw new RuntimeException("No more cups!"); }
+        this.nCoffeeOunces -= size;
+        this.nSugarPackets -= nSugarPackets;
+        this.nCreams -= nCreams;
+        this.nCups -= 1;
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
+        this.nCups += nCups;
+    }
+
     public static void main(String[] args) {
         new Cafe();
     }
+    
     
 }
--- Library.java
+++ Library.java
@@ -1,9 +1,55 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Enumeration;
 
-    public Library() {
+import javax.sound.sampled.SourceDataLine;
+
+public class Library extends Building{
+
+  private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
+
+    public void addTitle(String title) {
+      this.collection.put(title, true);
+    }
+
+    public String removeTitle(String title) {
+      this.collection.remove(title);
+      return title;
+    } // return the title that we removed
+
+    public void checkOut(String title) {
+      this.collection.replace(title, false);
+    }
+
+    public void returnBook(String title) {
+      this.collection.replace(title, true);
+    }
+
+    public boolean containsTitle(String title) {
+      return this.collection.containsKey(title);
+    } // returns true if the title appears as a key in the Libary's collection, false otherwise
+
+    public boolean isAvailable(String title) {
+      return this.collection.get(title);
+    } // returns true if the title is currently available, false otherwise
+
+    public void printCollection() {
+      Enumeration<String> keyEmun = this.collection.keys();
+      Enumeration<Boolean> valueEmun = this.collection.elements();
+      while (keyEmun.hasMoreElements() && valueEmun.hasMoreElements()) {
+        System.out.println("Title and Author: " + keyEmun.nextElement()+ ", " + "Checkout Status: " + valueEmun.nextElement());
+      }
+
+
+    } // prints out the entire collection in an easy-to-read way (including checkout status)
   
     public static void main(String[] args) {
       new Library();
